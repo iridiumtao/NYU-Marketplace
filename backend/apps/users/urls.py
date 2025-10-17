eef96f3a2369eaa348fb828832641a17a46b5b0d
Akhil Vardhan <akhilvardhan27@gmail.com>
2025-10-17 02:32:35 -0400

    Feat: Add URL routing for user authentication

---
+++ backend/apps/users/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from apps.users.views import AuthViewSet
+
+router = DefaultRouter()
+router.register('auth', AuthViewSet, basename='auth')
+urlpatterns = router.urls
+
+"""
+    Following APIs are Supported:
+
+       METHOD       API Endpoints              Function
+
+    1. POST         /api/auth/login/           Login or register user (auto-detects)
+    2. GET          /api/auth/me/              Get current authenticated user details
+"""
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('apps.users.urls')),
     path('api/', include('apps.listings.urls')),
     path("admin/", admin.site.urls),
 ]
