d49ddf38372f1259be383d8dc6402bbea5c628c1
iridiumtao <ct3354@nyu.edu>
2025-10-27 14:10:28 -0400

    feat: create unit test suite for backend

--- backend/apps/listings/tests.py
+++ backend/apps/listings/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from tests.factories.factories import ListingFactory, ListingImageFactory
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_listing_creation():
+    """
+    Test that a Listing instance can be created successfully.
+    """
+    listing = ListingFactory()
+    assert listing.pk is not None
+    assert "@nyu.edu" in listing.user.email
+
+
+@pytest.mark.django_db
+def test_listing_image_creation():
+    """
+    Test that a ListingImage instance can be created successfully.
+    """
+    listing_image = ListingImageFactory()
+    assert listing_image.pk is not None
+    assert listing_image.listing is not None
