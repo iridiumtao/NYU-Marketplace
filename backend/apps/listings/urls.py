20ef8cb196b834f8b751c0be885a811a7e8c43cb
Rohit Farfad <rohitfarfad@gmail.com>
2025-10-30 17:51:28 -0400

    Added search api description in urls.py

--- backend/apps/listings/urls.py
+++ backend/apps/listings/urls.py
@@ -8,15 +8,16 @@ urlpatterns = router.urls
 """
     Following APIs are Supported:
 
-       METHOD    AUTH    API Endpoints                  Function                     Fields
+       METHOD    AUTH    API Endpoints                           Function                     Fields
 
-    1. POST      Y       /api/v1/listings/             create a listing             category, title, description, price, status, location, images (optional, max 10)
-    2. GET       N       /api/v1/listings/             list all listings            listing_id, category, title, price, status, primary_image
-    3. GET       N       /api/v1/listings/<id>/        retrieve a single listing    listing_id, category, title, description, price, status, location, created_at, updated_at, images, user_email, user_netid
-    4. PUT/PATCH Y*      /api/v1/listings/<id>/        update a listing             category, title, description, price, status, location, new_images (optional), remove_image_ids (optional), update_images (optional)
-    5. DELETE    Y*      /api/v1/listings/<id>/        delete a listing             (deletes listing and all associated images from S3)
-    6. GET       Y       /api/v1/listings/user/        get user's listings          listing_id, category, title, price, status, primary_image
-
+    1. POST      Y       /api/v1/listings/                      create a listing             category, title, description, price, status, location, images (optional, max 10)
+    2. GET       N       /api/v1/listings/                      list all listings            listing_id, category, title, price, status, primary_image
+    3. GET       N       /api/v1/listings/<id>/                 retrieve a single listing    listing_id, category, title, description, price, status, location, created_at, updated_at, images, user_email, user_netid
+    4. PUT/PATCH Y*      /api/v1/listings/<id>/                 update a listing             category, title, description, price, status, location, new_images (optional), remove_image_ids (optional), update_images (optional)
+    5. DELETE    Y*      /api/v1/listings/<id>/                 delete a listing             (deletes listing and all associated images from S3)
+    6. GET       Y       /api/v1/listings/user/                 get user's listings          listing_id, category, title, price, status, primary_image
+    7. GET       Y       /api/v1/listings/search/q=<query>      search listings              listing_id, category, title, price, status, primary_image
+    
     * AUTH Y with OWNERSHIP CHECK: User must be authenticated AND own the listing
 
     Authentication:
