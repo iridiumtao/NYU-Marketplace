d15c9557c89c277ba593e903099e33659edacba7
Liam <herryson5896@gmail.com>
2025-10-29 21:30:28 -0400

    Add production/development build options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // production or development
+  base: isProduction ? '/static/' : '/',
+  
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+
+  build: {
+    // Output dir：Put build files into ../backend/static
+    outDir: path.resolve(__dirname, "../backend/frontend_build"),
+    emptyOutDir: true, // Empty before build
+  },
+
   // Vitest configuration
   test: {
     globals: true, // Allows using describe, it, expect etc. globally
